c97ba8fb2eb1128a5665cca31e9822804dbd5111
Momomash <momomash110697@gmail.com>
2022-04-20 16:18:34 +0300

    add absolute imports into tsconfig.json

--- src/core/router/Router.tsx
+++ src/core/router/Router.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Layout from '../../layouts/Layout';
-import { MainPage, GamePage, NotFoundPage } from '../../pages';
+import Layout from 'layouts/Layout';
+import { MainPage, GamePage, NotFoundPage } from 'pages';
 
 function Router() {
     return (
